13c649a486a9c1d4401dfe251356a34d58790cb0
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-06 01:21:20 +0800

    feat(auth): move Supabase credentials to env and refactor imports
    - Moved Supabase URL and anon key to `.env` file for security
    - Created `supabaseClient.js` to initialize Supabase client

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "/src/components/pages/Home";
 import { Login } from "/src/components/pages/Auth/Login";
 import { SignUp } from "/src/components/pages/Auth/SignUp";
-
-
-const supabase = createClient(
-  "https://ajafksdeulckwiaqtzuj.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqYWZrc2RldWxja3dpYXF0enVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1ODY2MjcsImV4cCI6MjA1NjE2MjYyN30.EA9JjEL8padkav3yQNV0RzlpMpnpvHXD8UzkOAs5Dvw "
-);
 
 function App() {
   return (
