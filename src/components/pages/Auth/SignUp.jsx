83ac13b2f45744c370df0a911e2cdaf3d2be24b3
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-07 12:17:27 +0800

    feat(auth): add form submission and error handling to signup
    - Use `useRef` to handle email and password inputs
    - Implement `handleSubmit` to process user signup with Supabase
    - Display error messages when signup fails
    - Improve `<Link>` structure for better readability

--- src/components/pages/Auth/SignUp.jsx
+++ src/components/pages/Auth/SignUp.jsx
@@ -1,15 +1,45 @@
 import "./Auth.css";
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useRef } from "react";
+import { useAuth } from "src/context/AuthContext";
 
 export function SignUp() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  const [error, setError] = useState(null)
+  const [message, setMessage] = useState("")
+
+  const {signUp} = useAuth()
+  let navigate = useNavigate()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    const email = emailRef.current.value
+    const password = passwordRef.current.value
+console.log("aaaaaaaaaa", { email, password });
+    const {error} = await signUp({email, password})
+      console.log("error before", error);
+    
+    if (error) {
+      setError(error)
+      setMessage("error with email or password")
+      console.log('error', error)
+      return
+    }
+    navigate("/")
+  }
+
   return (
     <div className="text-center div">
-      <form className="form-signin">
+      <form className="form-signin" onSubmit={handleSubmit}>
         <h1 className="h3 mb-3 font-weight-normal">Please sign up</h1>
         <label htmlFor="inputEmail" className="sr-only">
           Email address
         </label>
         <input
+          ref={emailRef}
           type="email"
           id="inputEmail"
           className="form-control"
@@ -20,6 +50,7 @@ export function SignUp() {
           Password
         </label>
         <input
+          ref={passwordRef}
           type="password"
           id="inputPassword"
           className="form-control"
@@ -34,8 +65,11 @@ export function SignUp() {
         <button className="btn btn-lg btn-primary btn-block" type="submit">
           Sign up
         </button>
-        <Link to="/login"> <a> Login</a></Link>
+        <Link to="/login">
+          <a> Login</a>
+        </Link>
       </form>
+      {message ? <p>{message}</p> : ""}
     </div>
   );
 }
