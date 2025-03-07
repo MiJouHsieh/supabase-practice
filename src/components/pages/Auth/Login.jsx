240c03cf0cb7c14d883a39ad9ebf7a54f0f0c3b1
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-07 12:38:00 +0800

    feat(auth): add authentication context for login
    - Created `AuthContext` to manage authentication state.
    - Implemented `signUp` and `signIn` functions.
    - Wrapped the application with `AuthProvider` in `App.js`.
    - Updated `Login.jsx` to use `useAuth` for authentication.
    - Refactored form handling with `useRef` for email and password fields.

--- src/components/pages/Auth/Login.jsx
+++ src/components/pages/Auth/Login.jsx
@@ -1,15 +1,41 @@
 import "./Auth.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useRef } from "react";
+import { useAuth } from "src/context/AuthContext";
 
 export function Login() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  const [error, setError] = useState(null)
+  const [message, setMessage] = useState("")
+
+  const {signUp} = useAuth()
+  let navigate = useNavigate()
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const { error } = await signUp({ email, password });
+
+    if (error) {
+      setError(error);
+      setMessage("error with email or password");
+      return;
+    }
+    navigate("/");
+  }
   return (
     <div className="text-center div">
-      <form className="form-signin">
+      <form className="form-signin" onSubmit={handleSubmit}>
         <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
         <label htmlFor="inputEmail" className="sr-only">
           Email address
         </label>
         <input
+          ref={emailRef}
           type="email"
           id="inputEmail"
           className="form-control"
@@ -20,6 +46,7 @@ export function Login() {
           Password
         </label>
         <input
+          ref={passwordRef}
           type="password"
           id="inputPassword"
           className="form-control"
@@ -34,8 +61,11 @@ export function Login() {
         <button className="btn btn-lg btn-primary btn-block" type="submit">
           Sign in
         </button>
-        <Link to="/signup"><a>Sign up</a></Link>
+        <Link to="/signup">
+          <a>Sign up</a>
+        </Link>
       </form>
+      {message ? <p>{message}</p> : ""}
     </div>
   );
 }
