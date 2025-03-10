7304b65f16b6e7b810eac1cee121302e39255e57
MiJouHsieh <mijouhsieh@gmail.com>
2025-03-10 21:17:18 +0800

    feat(nav): integrate authentication and improve navigation
    - Add `useAuth` to retrieve user authentication state and sign-out function
    - Replace anchor `<a>` tags with `Link` from `react-router-dom`
    - Implement `handleSignOut` to log users out and navigate to home page

---
+++ src/components/layouts/Nav.jsx
@@ -0,0 +1,75 @@
+import { useAuth } from "src/context/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
+
+export function Nav() {
+  const { user, signOut } = useAuth();
+  let navigate = useNavigate();
+
+  async function handleSignOut() {
+    await signOut();
+    navigate("/");
+  }
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
+      <div className="container px-4 px-lg-5">
+        <a className="navbar-brand" href="index.html">
+          Start Bootstrap
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i className="fas fa-bars"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ms-auto py-4 py-lg-0">
+            <li className="nav-item">
+              <a className="nav-link px-lg-3 py-3 py-lg-4" href="index.html">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link px-lg-3 py-3 py-lg-4" href="about.html">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link px-lg-3 py-3 py-lg-4" href="post.html">
+                Sample Post
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">
+                Contact
+              </a>
+            </li>
+            {user ? (
+              <li className="nav-item">
+                <a
+                  className="nav-link px-lg-3 py-3 py-lg-4"
+                  href="/"
+                  onClick={handleSignOut}
+                >
+                  Signout
+                </a>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <Link className="nav-link px-lg-3 py-3 py-lg-4" to="/login">
+                  Login
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
